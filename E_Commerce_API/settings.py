c5d460140593d5474ed13d4fc6ea3bd1e60f1669
Shoan <shaonmohammad13@gmail.com>
2025-02-22 12:58:22 +0600

    change the root dir of media file

--- E_Commerce_API/settings.py
+++ E_Commerce_API/settings.py
@@ -145,6 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 
 # Media files (Uploaded files like images)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/' 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
